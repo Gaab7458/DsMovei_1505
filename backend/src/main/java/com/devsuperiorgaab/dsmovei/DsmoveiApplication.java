708c1cc4093a5afcba99583349d28d5b4b0a2aef
Gaab Pereira <gabisouza250804@gmail.com>
2022-05-15 16:57:05 -0300

    create project

---
+++ backend/src/main/java/com/devsuperiorgaab/dsmovei/DsmoveiApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperiorgaab.dsmovei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoveiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoveiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperiorgaab/dsmovei/DsmoveiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperiorgaab.dsmovei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoveiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
